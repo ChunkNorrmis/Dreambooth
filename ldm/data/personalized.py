f368008dd7b1b1a9e52ca76f5401bb065cf68173
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-02-16 21:53:44 -0500

    Refactor PersonalizedBase class initialization and methods

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -1,49 +1,50 @@
 import os
 from typing import OrderedDict
 import numpy as np
-import PIL
 from PIL import Image
+from PIL.ImageEnhance import Sharpness as sharpen
 from torch.utils.data import Dataset
-from torchvision import transforms
 from captionizer import caption_from_path, generic_captions_from_path
 from captionizer import find_images
+import random
+
+
 
 per_img_token_list = [
     'א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י', 'כ', 'ל', 'מ', 'נ', 'ס', 'ע', 'פ', 'צ', 'ק', 'ר', 'ש', 'ת',
 ]
 
 class PersonalizedBase(Dataset):
-    def __init__(self,
-                 data_root,
-                 size=None,
-                 repeats=100,
-                 interpolation="bicubic",
-                 flip_p=0.5,
-                 set="train",
-                 placeholder_token="dog",
-                 per_image_tokens=False,
-                 center_crop=False,
-                 mixing_prob=0.25,
-                 coarse_class_text=None,
-                 token_only=False,
-                 reg=False
-                 ):
+    def __init__(
+        self,
+        data_root,
+        size,
+        repeats,
+        interpolation="bicubic",
+        flip_p=0.5,
+        set="train",
+        placeholder_token="dog",
+        per_image_tokens=False,
+        center_crop=False,
+        mixing_prob=0.25,
+        coarse_class_text=None,
+        token_only=False,
+        reg=False
+    ):
 
         self.data_root = data_root
-
         self.image_paths = find_images(self.data_root)
-
-        # self._length = len(self.image_paths)
         self.num_images = len(self.image_paths)
         self._length = self.num_images
-
         self.placeholder_token = placeholder_token
         self.token_only = token_only
         self.per_image_tokens = per_image_tokens
         self.center_crop = center_crop
         self.mixing_prob = mixing_prob
-
+        self.flip_p = flip_p
         self.coarse_class_text = coarse_class_text
+        self.size = size
+        self.reg = reg
 
         if per_image_tokens:
             assert self.num_images < len(
@@ -52,16 +53,17 @@ class PersonalizedBase(Dataset):
         if set == "train":
             self._length = self.num_images * repeats
 
-        self.size = size
-        self.interpolation = {"linear": PIL.Image.LINEAR,
-                              "bilinear": PIL.Image.BILINEAR,
-                              "bicubic": PIL.Image.BICUBIC,
-                              "lanczos": PIL.Image.LANCZOS,
-                              }[interpolation]
-        self.flip = transforms.RandomHorizontalFlip(p=flip_p)
-        self.reg = reg
+        self.interpolation = {
+            "nearest": 0,
+            "bilinear": 2,
+            "bicubic": 3,
+            "lanczos": 1,
+        }[interpolation]
+
         if self.reg and self.coarse_class_text:
             self.reg_tokens = OrderedDict([('C', self.coarse_class_text)])
+
+
 
     def __len__(self):
         return self._length
@@ -80,21 +82,25 @@ class PersonalizedBase(Dataset):
         else:
             example["caption"] = caption_from_path(image_path, self.data_root, self.coarse_class_text, self.placeholder_token)
 
-        # default to score-sde preprocessing
-        img = np.array(image).astype(np.uint8)
+        if self.center_crop and image.width != image.height:
+            im = np.array(image).astype(np.uint8)
+            H, W = im.shape[0], im.shape[1]
+            crop = min(W, H)
+            im = im[(H - crop) // 2: (H + crop) // 2, (W - crop) // 2: (W + crop) // 2]
+            image = Image.fromarray(im)
 
-        if self.center_crop:
-            crop = min(img.shape[0], img.shape[1])
-            h, w, = img.shape[0], img.shape[1]
-            img = img[(h - crop) // 2:(h + crop) // 2,
-                      (w - crop) // 2:(w + crop) // 2]
-
-        image = Image.fromarray(img)
         if self.size is not None:
-            image = image.resize((self.size, self.size),
-                                 resample=self.interpolation)
+            if image.width > self.size or image.height > self.size:
+                image = image.resize((self.size, self.size), resample=self.interpolation, reducing_gap=3)
 
-        image = self.flip(image)
-        image = np.array(image).astype(np.uint8)
-        example["image"] = (image / 127.5 - 1.0).astype(np.float32)
+        if random.random() < self.flip_p:
+            image = random.choice([
+                image.transpose(method=random.randint(0, 4)),
+                sharpen(image).enhance(random.uniform(-1.0, 2.0))
+            ])
+
+        img = np.array(image).astype(np.uint8)
+        img = (img / 127.5 - 1).astype(np.float32)
+        example['image'] = img
+
         return example
